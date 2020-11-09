66ac1f13d6e6b83a507d963f2a4392cd539b3cac
Paul Gomez <pauldro952@hotmail.com>
2020-11-09 16:02:17 -0600

    Update lookup-cstk.php

--- site/templates/lookup-cstk.php
+++ site/templates/lookup-cstk.php
@@ -1,20 +1,19 @@
 <?php
-		$filter = $modules->get('FilterCstk');
-		$filter->init_query($user);
-		$q = $input->get->text('q');
+	$filter = $modules->get('FilterCstk');
+	$filter->init_query($user);
+	$q = $input->get->text('q');
 
-		if ($input->get->q) {
-			$filter->search($q);
-			$page->headline = "Searching for '$q'";
-		}
+	if ($input->get->q) {
+		$filter->search($q);
+		$page->headline = "Searching for '$q'";
+	}
 
-		$filter->sortby($page);
-		$query = $filter->get_query();
-		$cstks = $query->paginate($input->pageNum, 10);
+	$filter->sortby($page);
+	$query = $filter->get_query();
+	$cstks = $query->paginate($input->pageNum, 10);
 
-		$page->body .= $config->twig->render('api/lookup/cstk/search.twig', ['page' => $page, 'cstks' => $cstks, 'datamatcher' => $modules->get('RegexMatcher'), 'q' => $q]);
-		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $cstks->getNbResults()]);
-
+	$page->body .= $config->twig->render('api/lookup/cstk/search.twig', ['page' => $page, 'cstks' => $cstks, 'datamatcher' => $modules->get('RegexMatcher'), 'q' => $q]);
+	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $cstks->getNbResults()]);
 
 	if ($config->ajax) {
 		echo $page->body;
